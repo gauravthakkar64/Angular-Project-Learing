84ef9fd1c07f5f6244dd3929ad4fb853e340a738
Gaurav Thakkar <gauravthakkar644@gmail.com>
2018-05-26 16:30:15 +0530

    Added Dependency Injection to the controllers

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -2,14 +2,27 @@
 
     angular.module("turtleFacts").controller("listCtrl", ListController);
 
-    function ListController(){
+    ListController.$inject = ['quizMetrics'];
+
+    function ListController(quizMetrics){
         var vm=this;
+    
+        vm.quizMetrics=quizMetrics;
         vm.data=turtlesData;
         vm.activeTurtle={};
         vm.changeActiveTurtle=changeActiveTurtle;
+        vm.activateQuiz=activateQuiz;    
         vm.search="";
+       
+
+
         function changeActiveTurtle(index){
             vm.activeTurtle=index;
+        }
+
+
+        function activateQuiz(){
+             quizMetrics.changeState(true);
         }
     }
     
